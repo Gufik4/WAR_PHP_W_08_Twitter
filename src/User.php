b101f7bd31c0ddf04fb4abfb81a7e0848feafe0b
Daniel Gustaw <gustaw.daniel@gmail.com>
2017-09-10 14:34:50 +0200

    Class user

--- src/User.php
+++ src/User.php
@@ -45,4 +45,71 @@ class User
 
         $this->hashedPass = $hashedPass;
     }
+
+    public static function getById(PDO $conn, $id) {
+        $stmt  =$conn->prepare("SELECT * FROM user WHERE id=:id");
+        $res = $stmt->execute(['id'=>$id]);
+        if($res) {
+            $array = $stmt->fetch(PDO::FETCH_ASSOC);
+            $user = new User();
+            $user->id = $array["id"];
+            $user->setName($array["name"]);
+            $user->setEmail($array["email"]);
+            $user->hashedPass = $array["hashed_pass"];
+            return $user;
+        }
+        return false;
+    }
+
+    public static function getAll(PDO $conn) {
+        $stmt  =$conn->query("SELECT * FROM user");
+        $users = [];
+        foreach ($stmt->fetchAll(PDO::FETCH_ASSOC) as $array) {
+            $user = new User();
+            $user->id = $array["id"];
+            $user->setName($array["name"]);
+            $user->setEmail($array["email"]);
+            $user->hashedPass = $array["hashed_pass"];
+            $users[] = $user;
+        }
+        return $users;
+    }
+
+    public function save(PDO $conn) {
+        if(!$this->id) {
+            $stmt = $conn->prepare("INSERT INTO user (email,name,hashed_pass)
+                      VALUES (:email,:name,:hashed_pass)");
+            $res = $stmt->execute([
+                'email'=>$this->getEmail(),
+                'name'=>$this->getName(),
+                'hashed_pass'=>$this->getHashedPass()
+            ]);
+            if($res) {
+                $this->id = $conn->lastInsertId();
+            }
+            } else {
+            $stmt = $conn->prepare("UPDATE user 
+            SET email = :email, name = :name, hashed_pass = :hashed_pass
+            WHERE id = :id;");
+            $res = $stmt->execute([
+                'email'=>$this->getEmail(),
+                'name'=>$this->getName(),
+                'hashed_pass'=>$this->getHashedPass(),
+                'id'=>$this->getId()
+            ]);
+        }
+        return (bool) $res;
+    }
+
+    public function delete(PDO $conn) {
+        if($this->getId()) {
+            $stmt = $conn->prepare("DELETE FROM user WHERE id=:id");
+            $res = $stmt->execute([
+                'id'=>$this->getId()
+            ]);
+            return (bool) $res;
+        }
+        return false;
+    }
+
 }
---
+++ test/test.php
@@ -0,0 +1,52 @@
+<?php
+
+require __DIR__."/../src/User.php";
+
+$conn = new PDO("mysql:host=localhost;dbname=twitter",'root',null,[
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+]);
+
+$conn->query(file_get_contents(__DIR__."/../sql/main.sql"));
+
+if(User::getById($conn,1)->getEmail() === "adam@spadam.pl") {
+    echo "ok\n";
+} else {
+    echo "no\n";
+}
+
+if(count(User::getAll($conn)) === 1) {
+    echo "ok\n";
+} else {
+    echo "no\n";
+}
+
+$user = new User();
+$user->setEmail("aleks@kleks.pl");
+$user->setName("Ala");
+$user->setHashedPass("pass");
+$user->save($conn);
+
+if(count(User::getAll($conn)) === 2) {
+    echo "ok\n";
+} else {
+    echo "no\n";
+}
+
+$user->setEmail("marek@zakamarek.pl");
+$user->save($conn);
+
+
+if((User::getById($conn,2))->getEmail() === "marek@zakamarek.pl") {
+    echo "ok\n";
+} else {
+    echo "no\n";
+}
+
+$user->delete($conn);
+unset($user);
+
+if(count(User::getAll($conn)) === 1) {
+    echo "ok\n";
+} else {
+    echo "no\n";
+}
